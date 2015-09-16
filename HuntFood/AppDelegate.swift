9d412eb039edffa5b86be15c322b1cd337b64aa9
noprom <tyee.noprom@qq.com>
2015-09-16 23:24:58 +0800

    setup image detail

--- HuntFood/AppDelegate.swift
+++ HuntFood/AppDelegate.swift
@@ -23,6 +23,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
         // 设置运营商等字体的颜色
         UIApplication.sharedApplication().statusBarStyle = .LightContent
+        // 设置详情页面的底部导航栏的颜色
+        UIBarButtonItem.appearance().tintColor = UIColor(red: 235.0/255.0, green: 73.0/255.0, blue:
+            27.0/255.0, alpha: 1.0)
+        UIToolbar.appearance().barTintColor = UIColor(red: 237.0/255.0, green: 240.0/255.0, blue:
+            243.0/255.0, alpha: 0.5)
         return true
     }
 
