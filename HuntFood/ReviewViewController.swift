ab577f64f4d1bb4910d9cef59e25e7bac5f9512a
noprom <tyee.noprom@qq.com>
2015-09-20 23:12:03 +0800

    add scale and transform animations

--- HuntFood/ReviewViewController.swift
+++ HuntFood/ReviewViewController.swift
@@ -22,14 +22,30 @@ class ReviewViewController: UIViewController {
         blurEffectView.frame = view.bounds
         backgroundImageView.addSubview(blurEffectView)
         
-        //初始化为点评框增加渐变效果
-        dialogView.transform = CGAffineTransformMakeScale(0.0, 0.0)
+        //初始化为点评框增加缩放效果
+        let scale = CGAffineTransformMakeScale(0.0, 0.0)
+        //一个滑动的动画
+        let transform = CGAffineTransformMakeTranslation(0, 500)
+        //组合动画效果
+        dialogView.transform = CGAffineTransformConcat(scale, transform)
     }
 
     override func viewWillAppear(animated: Bool) {
-        //为点评框增加渐变效果
-        UIView.animateWithDuration(0.7, delay: 0.0, options: nil, animations: {
-            self.dialogView.transform = CGAffineTransformMakeScale(1, 1)
+        //为点评框增加缩放效果
+//        UIView.animateWithDuration(0.7, delay: 0.0, options: nil, animations: {
+//            self.dialogView.transform = CGAffineTransformMakeScale(1, 1)
+//        }, completion: nil)
+        
+        // Spring animation
+//        UIView.animateWithDuration(0.7, delay: 0.0, usingSpringWithDamping: 0.6,
+//            initialSpringVelocity: 0.5, options: nil, animations: {
+//            self.dialogView.transform = CGAffineTransformMakeTranslation(0, 0)
+//            }, completion: nil)
+        UIView.animateWithDuration(0.7, delay: 0.0, usingSpringWithDamping: 0.6,
+            initialSpringVelocity: 0.5, options: nil, animations: {
+            let scale = CGAffineTransformMakeScale(1, 1)
+            let translate = CGAffineTransformMakeTranslation(0, 0)
+            self.dialogView.transform = CGAffineTransformConcat(scale, translate)
         }, completion: nil)
     }
     
